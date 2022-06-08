99ecae5c6e99590d1c2c1728a65c15fa8d57c88e
ozifirebrand <oziomaokoroafor@gmail.com>
2022-06-08 15:31:34 +0100

    Edit Home controller

--- src/main/java/ozi/app/printApp/web/controllers/HomeController.java
+++ src/main/java/ozi/app/printApp/web/controllers/HomeController.java
@@ -16,7 +16,7 @@ public class HomeController {
     @Autowired
     private OrderServices orderServices;
 
-    @PostMapping("")
+    @GetMapping("")
     public ResponseEntity<?> welcome(){
         return ResponseEntity.ok().body("Welcome to printApp");
     }
