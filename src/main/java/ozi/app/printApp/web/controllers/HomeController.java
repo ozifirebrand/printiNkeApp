df272123af6f917403c736e246a2ff0b2ae7b83b
ozifirebrand <oziomaokoroafor@gmail.com>
2022-06-09 12:34:13 +0100

    Controller update

--- src/main/java/ozi/app/printApp/web/controllers/HomeController.java
+++ src/main/java/ozi/app/printApp/web/controllers/HomeController.java
@@ -1,27 +1,14 @@
 package ozi.app.printApp.web.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import ozi.app.printApp.data.dtos.requests.OrderCreationRequest;
-import ozi.app.printApp.data.dtos.responses.OrderCreationResponse;
-import ozi.app.printApp.exceptions.BusinessLogicException;
-import ozi.app.printApp.services.orderService.OrderServices;
 
 @RestController
 @RequestMapping("/")
 
 public class HomeController {
-
-    @Autowired
-    private OrderServices orderServices;
-
     @GetMapping("")
     public ResponseEntity<?> welcome(){
         return ResponseEntity.ok().body("Welcome to printApp");
-    }
-    @GetMapping("/welcome-page")
-    public OrderCreationResponse makeOrder(@RequestBody OrderCreationRequest request) throws BusinessLogicException {
-        return orderServices.createOrder(request);
     }
 }
--- src/main/java/ozi/app/printApp/web/controllers/UserController.java
+++ src/main/java/ozi/app/printApp/web/controllers/UserController.java
@@ -2,9 +2,13 @@ package ozi.app.printApp.web.controllers;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import ozi.app.printApp.data.dtos.requests.OrderCreationRequest;
 import ozi.app.printApp.data.dtos.requests.UserCreationRequest;
+import ozi.app.printApp.data.dtos.responses.OrderCreationResponse;
+import ozi.app.printApp.exceptions.BusinessLogicException;
 import ozi.app.printApp.services.userService.UserServices;
 
 @Slf4j
@@ -14,6 +18,27 @@ public class UserController {
     @Autowired
     private UserServices userServices;
 
+    @PostMapping("/order")
+    public ResponseEntity<?> createOrder(@RequestBody OrderCreationRequest orderCreationRequest){
+        try {
+            OrderCreationResponse orderCreationResponse = userServices.createOrder(orderCreationRequest);
+            return ResponseEntity.status(HttpStatus.CREATED).body(orderCreationResponse);
+        }
+        catch (BusinessLogicException businessLogicException){
+            return ResponseEntity.badRequest().body(businessLogicException.getMessage());
+        }
+    }
+
+    @GetMapping("/order/{id}")
+    public ResponseEntity<?> getOrderById(@PathVariable String id){
+        try {
+            OrderCreationResponse orderCreationResponse = userServices.getOrderById(id);
+            return ResponseEntity.status(HttpStatus.OK).body(orderCreationResponse);
+        }
+        catch (BusinessLogicException exception){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception.getMessage());
+        }
+    }
 
     @GetMapping("/orders{id}")
     public ResponseEntity<?> viewAllOrders(@PathVariable String id){
